c8939d801537f469f79ac0d33e181c31ce2d5dc2
vely2564 <vely2564@gmail.com>
2021-07-24 14:55:12 +0700

    '1'

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,19 @@
+<script>
+	let count = 0; let doubled = 0;
+
+	function incrementCount() {
+    console.log('count');
+		count += 1;
+	}
+
+  $: {
+    console.log('doubled');
+    doubled = count * 2;
+  }
+</script>
+
+<button on:click={incrementCount}>
+	Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
+
+<p>doubled is {doubled}</p>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,10 @@
+<script>
+  import Nested from '$lib/Nested.svelte';
+  import Counter from '$lib/Counter.svelte';
+</script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Counter />
+
+<Nested name='TeSt' />
